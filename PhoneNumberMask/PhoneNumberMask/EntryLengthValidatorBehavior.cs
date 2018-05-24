fc419a83acd181d1444d0357adb5510f8bfa4af4
DESKTOP-KFRLMGS\SuneelPC <suneel.biyyapu@gmail.com>
2018-05-24 08:07:31 +0530

    Add project files.

---
+++ PhoneNumberMask/PhoneNumberMask/EntryLengthValidatorBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+
+namespace PhoneNumberMask
+{
+    public class EntryLengthValidator : Behavior<Entry>
+    {
+        public int MaxLength { get; set; }
+        public int MinLength { get; set; } = 0;
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = (Entry)sender;
+
+            // if Entry text is longer than valid length
+            if (entry.Text.Length > this.MaxLength)
+            {
+                string entryText = entry.Text;
+
+                entryText = entryText.Remove(entryText.Length - 1); // remove last char
+
+                entry.Text = entryText;
+            }
+
+            if (MinLength > 0)
+                if (entry.Text.Length < this.MinLength)
+                {
+                    ((Entry)sender).TextColor = Color.Red;
+                }
+                else
+                    ((Entry)sender).TextColor = Color.Black;
+        }
+    }
+}
---
+++ PhoneNumberMask/PhoneNumberMask/PhoneNumberMaskBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PhoneNumberMask
+{
+    public class PhoneNumberMaskBehavior : Behavior<Entry>
+    {
+        ///
+        /// Attaches when the page is first created.
+        /// 
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        ///
+        /// Detaches when the page is destroyed.
+        /// 
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                // If the new value is longer than the old value, the user is
+                if (args.OldTextValue != null && args.NewTextValue.Length < args.OldTextValue.Length)
+                    return;
+
+                var value = args.NewTextValue;
+
+                if (value.Length == 3)
+                {
+                    ((Entry)sender).Text += "-";
+                    return;
+                }
+
+                if (value.Length == 7)
+                {
+                    ((Entry)sender).Text += "-";
+                    return;
+                }
+
+                ((Entry)sender).Text = args.NewTextValue;
+            }
+        }
+    }
+}
